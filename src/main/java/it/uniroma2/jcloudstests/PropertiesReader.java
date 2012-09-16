98764fc71600fff64ba96c7273bd3bf019f62440
Pasquale Andriani <pasquale.andriani@gmail.com>
2012-09-16 16:36:14 +0200

    first logic

---
+++ src/main/java/it/uniroma2/jcloudstests/PropertiesReader.java
@@ -0,0 +1,21 @@
+package it.uniroma2.jcloudstests;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesReader extends Properties{
+
+	public enum AWS {
+		ACCESS_KEY_ID,
+		SECRET_KEY,
+		KEY_NAME,
+		SECURITY_GROUP_NAME
+	}
+
+	private static final long serialVersionUID = -1582779298930647956L;
+
+	public PropertiesReader(String resourceName) throws IOException{
+		load(getClass().getClassLoader().getResourceAsStream(resourceName));
+	}
+	
+}
